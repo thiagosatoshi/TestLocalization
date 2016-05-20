7bac9c95b0f0570d0bd69119e47bd28aaa73332c
Thiago Suzuki <thiagosatoshi@gmail.com>
2016-05-20 15:30:44 +0100

    First commit

---
+++ TestLocalization/TestLocalization/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TestLocalization
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Session != null)
+            {
+                CultureInfo cultureInfo = (CultureInfo)this.Session["Culture"];
+                if (cultureInfo == null)
+                {
+                    string langName = "en";
+
+                    if (HttpContext.Current.Request.UserLanguages != null && HttpContext.Current.Request.UserLanguages.Length != 0)
+				        langName = HttpContext.Current.Request.UserLanguages[0].Substring(0, 2);
+
+                    cultureInfo = new CultureInfo(langName);
+                    this.Session["Culture"] = cultureInfo;
+                }
+
+                //Finally setting culture for each request
+                Thread.CurrentThread.CurrentUICulture = cultureInfo;
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureInfo.Name);
+            }
+        }
+
+    }
+}
---
+++ TestLocalization/TestLocalization/Models/AccountViewModels.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestLocalization.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[Display(Name = "Email")]
+        [Display(Name = "Email", ResourceType = typeof(ViewRes.Names))]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class VerifyCodeViewModel
+    {
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        public string Provider { get; set; }
+
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        [Display(Name = "Code")]
+        //[Display(Name = "Code", ResourceType = typeof(ViewRes.Names))] ????
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
+
+        //[Display(Name = "Remember this browser?")]
+        [Display(Name = "RememberBrowser", ResourceType = typeof(ViewRes.Names))]
+        public bool RememberBrowser { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[Display(Name = "Email")]
+        [Display(Name = "Email", ResourceType = typeof(ViewRes.Names))]
+        public string Email { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[Display(Name = "Email")]
+        [Display(Name = "Email", ResourceType = typeof(ViewRes.Names))]
+        //[EmailAddress]
+        [EmailAddress(ErrorMessageResourceName = "EmailAddress", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        public string Email { get; set; }
+
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        [DataType(DataType.Password)]
+        //[Display(Name = "Password")]
+        [Display(Name = "Password", ResourceType = typeof(ViewRes.Names))]
+        public string Password { get; set; }
+
+        //[Display(Name = "Remember me?")]
+        [Display(Name = "RememberMe", ResourceType = typeof(ViewRes.Names))]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[EmailAddress]
+        [EmailAddress(ErrorMessageResourceName = "EmailAddress", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[Display(Name = "Email")]
+        [Display(Name = "Email", ResourceType = typeof(ViewRes.Names))]
+        public string Email { get; set; }
+
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessageResourceName = "PasswordMinLength", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings), MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        //[Display(Name = "Password")]
+        [Display(Name = "Password", ResourceType = typeof(ViewRes.Names))]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        //[Display(Name = "Confirm password")]
+        [Display(Name = "ConfirmPassword", ResourceType = typeof(ViewRes.Names))]
+        //[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessageResourceName = "PasswordMustMatch", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[EmailAddress]
+        [EmailAddress(ErrorMessageResourceName = "EmailAddress", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[Display(Name = "Email")]
+        [Display(Name = "Email", ResourceType = typeof(ViewRes.Names))]
+        public string Email { get; set; }
+
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessageResourceName = "PasswordMinLength", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings), MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        //[Display(Name = "Password")]
+        [Display(Name = "Password", ResourceType = typeof(ViewRes.Names))]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        //[Display(Name = "Confirm password")]
+        [Display(Name = "ConfirmPassword", ResourceType = typeof(ViewRes.Names))]
+        //[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessageResourceName = "PasswordMustMatch", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        //[Required]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[EmailAddress]
+        [EmailAddress(ErrorMessageResourceName = "EmailAddress", ErrorMessageResourceType = typeof(ViewRes.ValidationStrings))]
+        //[Display(Name = "Email")]
+        [Display(Name = "Email", ResourceType = typeof(ViewRes.Names))]
+        public string Email { get; set; }
+    }
+}
